f2bad3cb68cc9e8d30fd6677ef0d2b2807e06c57
KangHuijun <khj50231@gmail.com>
2025-05-21 16:51:22 +0900

    controller

--- nest/src/People/people.controller.ts
+++ nest/src/People/people.controller.ts
@@ -8,18 +8,42 @@ import {
   Delete,
 } from '@nestjs/common';
 import { CreatePeopledto } from './create-people.dto';
+import { PeopleService } from './people.service';
+import { UpdatePeopleDto } from './update-people.dto';
 
-@Controller()
+@Controller('people')
 export class PeopleController{
-    constructor(private readonly postService: PostService) {}
-    @Get()
-
+    constructor(private readonly peopleService: PeopleService) {}
     @Post()
-
-    @Patch()
-
-    @Delete()
-
-  controller(get, post, patch, delete), service(db 접속 할 수 있도록)
+    async create(@Body() createP: CreatePeopledto){
+      await this.peopleService.create(createP);
+      return {
+        message: 'People created successfully'
+      };
+    }
+    @Get() //Get은 정보 가져오기 전체 가져오기 명령에는 매개변수 없어도 됨
+    async findAll(){
+      return {
+        data: await this.peopleService.findAll()
+      };
+    }
+    @Get(':id')
+    async findOne(@Param('id') id: string){//내부 엔티티나 dto에선 숫자로 받았지만 주소에서는 문자열로 왔음 ㅇㅇ
+      return await this.peopleService.findOne(+id); //+로 형변환
+    }
+    @Patch(':id')
+    async update(@Param('id') id: string){
+      await this.peopleService.update(+id, UpdatePeopleDto)
+      return {
+        message: 'People updated successfully'
+      };
+      }
+    @Delete(':id')
+    async remove(@Param('id') id: string){
+      await this.peopleService.remove(+id);
+      return {
+        message: 'People deleted successfully'
+      };
+    }
 
 }
